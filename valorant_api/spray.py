bfbd75ca97525550c824debe05a0ec1eb4d322f8
MountainFlash <65584814+MinshuG@users.noreply.github.com>
2021-02-19 19:24:34 +0530

    updated spray class

--- valorant_api/spray.py
+++ valorant_api/spray.py
@@ -6,13 +6,17 @@ class Spray:
     display_name: str
     category: Optional[str]
     display_icon: str
+    full_icon: Optional[str]
+    full_transparent_icon: Optional[str]
     asset_path: str
     raw_data: dict
 
-    def __init__(self,data: dict) -> None:
+    def __init__(self, data: dict) -> None:
         self.uuid = data.get("uuid")
         self.display_name = data.get("displayName")
         self.category = data.get("category")
         self.display_icon = data.get("displayIcon")
+        self.full_icon = data.get("fullIcon")
+        self.full_transparent_icon = data.get("fullTransparentIcon")
         self.asset_path = data.get("assetPath")
         self.raw_data = data
