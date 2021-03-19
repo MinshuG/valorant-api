891c1998304f31a0c66fc8d6d5779a1c67aeb96c
MinshuG <65584814+MinshuG@users.noreply.github.com>
2021-03-19 21:54:38 +0530

    oops

--- valorant_api/buddies.py
+++ valorant_api/buddies.py
@@ -1,4 +1,19 @@
-from typing import Optional
+from typing import List, Optional
+
+
+class Level:
+    uuid: str
+    charm_level: int
+    display_name: Optional[str]
+    display_icon: Optional[str]
+    asset_path: str
+
+    def __init__(self, data: dict) -> None:
+        self.uuid = data.get("uuid")
+        self.charm_level = data.get("charmLevel")
+        self.display_name = data.get("displayName")
+        self.display_icon = data.get("displayIcon")
+        self.asset_path = data.get("assetPath")
 
 
 class Buddy:
@@ -8,6 +23,7 @@ class Buddy:
     theme_uuid: None
     display_icon: str
     asset_path: str
+    levels: List[Level]
     raw_data: dict
 
     def __init__(self, data: dict):
@@ -17,22 +33,8 @@ class Buddy:
         self.theme_uuid = data.get("themeUuid")
         self.display_icon = data.get("displayIcon")
         self.asset_path = data.get("assetPath")
+        self.levels = [Level(x) for x in data.get("levels")]
         self.raw_data = data
 
     def __str__(self):
         return str(self.raw_data)
-
-
-class Level:
-    uuid: str
-    spray_level: int
-    display_name: Optional[str]
-    display_icon: Optional[str]
-    asset_path: str
-
-    def __init__(self, data: dict) -> None:
-        self.uuid = data.get("uuid")
-        self.spray_level = data.get("charmLevel")
-        self.display_name = data.get("displayName")
-        self.display_icon = data.get("displayIcon")
-        self.asset_path = data.get("assetPath")
--- valorant_api/sprays.py
+++ valorant_api/sprays.py
@@ -1,29 +1,4 @@
-from typing import Optional
-
-
-class Spray:
-    uuid: str
-    display_name: str
-    category: Optional[str]
-    display_icon: str
-    full_icon: Optional[str]
-    full_transparent_icon: Optional[str]
-    asset_path: str
-    raw_data: dict
-
-    def __init__(self, data: dict) -> None:
-        self.uuid = data.get("uuid")
-        self.display_name = data.get("displayName")
-        self.category = data.get("category")
-        self.display_icon = data.get("displayIcon")
-        self.full_icon = data.get("fullIcon")
-        self.full_transparent_icon = data.get("fullTransparentIcon")
-        self.asset_path = data.get("assetPath")
-        self.levels = [Level(x) for x in data.get("levels")]
-        self.raw_data = data
-
-    def __str__(self):
-        return str(self.raw_data)
+from typing import Optional, List
 
 
 class Level:
@@ -39,3 +14,29 @@ class Level:
         self.display_name = data.get("displayName")
         self.display_icon = data.get("displayIcon")
         self.asset_path = data.get("assetPath")
+
+
+class Spray:
+    uuid: str
+    display_name: str
+    category: Optional[str]
+    display_icon: str
+    full_icon: Optional[str]
+    full_transparent_icon: Optional[str]
+    asset_path: str
+    levels: List[Level]
+    raw_data: dict
+
+    def __init__(self, data: dict) -> None:
+        self.uuid = data.get("uuid")
+        self.display_name = data.get("displayName")
+        self.category = data.get("category")
+        self.display_icon = data.get("displayIcon")
+        self.full_icon = data.get("fullIcon")
+        self.full_transparent_icon = data.get("fullTransparentIcon")
+        self.asset_path = data.get("assetPath")
+        self.levels = [Level(x) for x in data.get("levels")]
+        self.raw_data = data
+
+    def __str__(self):
+        return str(self.raw_data)
